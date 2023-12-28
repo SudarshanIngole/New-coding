d06043c9e2a47343d4e198d5de074ce11b0bc915
Sudarshan Ingole <153209641+SudarshanIngole@users.noreply.github.com>
2023-12-28 12:54:13 +0530

    Assignment

---
+++ Daily Assignment in DM (2).py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+# Creating two arrays
+array1 = np.array([[1, 2], [3, 4]])
+array2 = np.array([[5, 6], [7, 8]])
+
+# Performing dot product
+result = np.dot(array1, array2)
+
+print(result)
+
+
+# In[3]:
+
+
+# adding two arrays
+array1 = np.array([[1, 8], [3, 9]])
+array2 = np.array([[4, 6], [2, 8]])
+
+
+result = (array1 + array2)
+
+print(result)
+
+
+# In[10]:
+
+
+np.linspace(2,50, retstep=True)
+
+
+# In[15]:
+
+
+import numpy as np
+
+                               # Create a 8x7 array of ones
+ones_array = np.ones((8, 7))
+
+print(ones_array)
+
+
+# In[16]:
+
+
+np.full((3,3),55)
+
+
+# In[1]:
+
+
+import numpy as np
+arr = np.array([2, 4, 6, 8, 10])
+res = arr[::-1]
+print(res)
+
+
+# In[ ]:
+
+
+
+
